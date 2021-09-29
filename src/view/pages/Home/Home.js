e3a1dadad9ef0b07d3743153fb32583c9155556b
Apoquinto <RCLlanesA@gmail.com>
2021-09-29 01:06:22 -0500

    :sparkles: Make pages templete literals

---
+++ src/view/pages/Home/Home.js
@@ -0,0 +1,6 @@
+export const home = `
+    <h2>Mis pendientes</h2>
+    <p>
+        Esta es una lista en la que puedes fijar tus peliculas favoritas de tú colección.
+    </p>
+`;
---
+++ src/view/pages/MovieList/MovieList.js
@@ -0,0 +1,20 @@
+export const movieList = `
+<h2>Listado</h2>
+<table>
+  <thead>
+    <tr>
+      <th>Id</th>
+      <th>Nombre</th>
+      <th>Puntuación</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Id</td>
+      <td>Nombre</td>
+      <td>Puntuación</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+`;
