fec55dbab294225005221e190cc396da7182389c
Apoquinto <RCLlanesA@gmail.com>
2021-09-29 02:45:15 -0500

    :lipstick: Change title

--- src/view/pages/Home/Home.js
+++ src/view/pages/Home/Home.js
@@ -1,5 +1,5 @@
 export const home = `
-    <h2>Mis pendientes</h2>
+    <h2>Mis favoritos</h2>
     <p>
         Esta es una lista en la que puedes fijar tus peliculas favoritas de tú colección.
     </p>
