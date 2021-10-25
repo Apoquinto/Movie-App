44dc43b1efd3473080fa0555ba97c23754b3fb9b
Apoquinto <RCLlanesA@gmail.com>
2021-10-24 20:09:16 -0500

    :sparkles: Add collection

--- src/ctrl/controller.js
+++ src/ctrl/controller.js
@@ -14,6 +14,11 @@ let collections = [
       keyPath: "id",
       autoIncrement: true,
     },
+    name: "users",
+    config: {
+      keyPath: "id",
+      autoIncrement: true,
+    },
   },
 ];
 let data = new IndexedDBManager("App Movies", 1, collections);
